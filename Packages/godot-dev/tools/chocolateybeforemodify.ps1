f5d767a6783ec8ecb6ec505a07eedb933c83b4a4
ルパン三世 <148364322+Bonddy97@users.noreply.github.com>
2023-10-22 16:14:22 +0800

    Godot: update scripts.

--- Packages/godot-dev/tools/chocolateybeforemodify.ps1
+++ Packages/godot-dev/tools/chocolateybeforemodify.ps1
@@ -1,10 +1,4 @@
-﻿# This runs before upgrade or uninstall.
-# Use this file to do things like stop services prior to upgrade or uninstall.
-# NOTE: It is an anti-pattern to call chocolateyUninstall.ps1 from here. If you
-#  need to uninstall an MSI prior to upgrade, put the functionality in this
-#  file without calling the uninstall script. Make it idempotent in the
-#  uninstall script so that it doesn't fail when it is already uninstalled.
-# NOTE: For upgrades - like the uninstall script, this script always runs from
-#  the currently installed version, not from the new upgraded package version.
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Uninstall-ChocolateyZipPackage $packageName 'Godot*.zip'
+﻿$ErrorActionPreference = "Stop";
+
+# Stop-Process won't error if the process doesn't exist
+Get-Process "Godot Engine" -ErrorAction SilentlyContinue | Stop-Process -ErrorAction Stop
--- Packages/godot-dev/tools/chocolateyinstall.ps1
+++ Packages/godot-dev/tools/chocolateyinstall.ps1
@@ -1,16 +1,64 @@
-﻿$ErrorActionPreference = 'Stop' # stop on all errors
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿# Godot_v$version_$arch.zip
+# └── Godot_v$version_$arch.exe
+# └── Godot_v$version_$arch_console.exe
+  
+# Release URL: https://github.com/godotengine/godot-builds/releases
+# And in this package, I just pick the pre-release.
+#$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = "Godot-dev"
+# For win32
 $url = 'https://github.com/godotengine/godot-builds/releases/download/4.2-beta2/Godot_v4.2-beta2_win32.exe.zip'
 $checksum = '7383518fd219695afb04ff5e189ce88151e441f3d2a9460967b87fa639bbc30e'
+# For win64
 $url64 = 'https://github.com/godotengine/godot-builds/releases/download/4.2-beta2/Godot_v4.2-beta2_win64.exe.zip'
 $checksum64 = '5406da75237aa31b4a37a40f5e4e2c552d5494736f957fa14d254cca328dd8e7'
-# $env:ChocolateyPackageName
-Install-ChocolateyZipPackage `
-  -PackageName "$packageName" `
-  -Url $url `
-  -UnzipLocation "$toolsDir" `
-  -Url64bit "$url64" `
-  -Checksum "$checksum" `
-  -ChecksumType "sha256" `
-  -Checksum64 "$checksum64" `
-  -ChecksumType64 "sha256"
+# Define unizip location.
+$unzipLocation = Join-Path ([Environment]::GetFolderPath("LocalApplicationData")) $packageName
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = "ZIP"
+  # For win32
+  url            = $url
+  checksum       = $checksum
+  checksumType   = "sha256"
+  # For win64
+  url64          = $url64
+  checksum64     = $checksum64
+  checksumType64 = "sha256"
+  # Unzip location
+  unzipLocation  = $unzipLocation
+}
+
+
+# Install the zip package
+Install-ChocolateyZipPackage @packageArgs
+
+# Get the unzip folder
+$folder = Get-ChildItem -Path $unzipLocation -Directory | Where-Object { $_.Name -like "Godot*" } | Select-Object -First 1
+if ($folder) {
+  # Copy conetnt to the unzip location
+  Copy-Item -Path "$($folder.FullName)\\*" -Destination $unzipLocation -Recurse -Force
+  # Remove unzip folder
+  Remove-Item -Path $folder.FullName -Recurse -Force
+}
+else {
+  Write-Output "No unzip folder found!"
+}
+
+# Create shortcuts
+$godot = Get-ChildItem -Path $unzipLocation -Recurse | Where-Object { $_.Name -match "Godot.*\.exe$" -and $_.Name -notmatch "console" } | Select-Object -ExpandProperty FullName | Select-Object -First 1
+$godot_console = Get-ChildItem -Path $unzipLocation -Recurse | Where-Object { $_.Name -match "Godot.*console\.exe$" } | Select-Object -ExpandProperty FullName | Select-Object -First 1
+
+@(
+    , @('Godot.dev', $godot)
+    , @('Godot.console.dev', $godot_console)
+) | ForEach-Object {
+    # Create Programs shortcuts
+    $programsShortcutPath = Join-Path ([Environment]::GetFolderPath("Programs")) "$($_[0]).lnk"
+    Install-ChocolateyShortcut -ShortcutFilePath $programsShortcutPath -Target $_[1] -WorkingDirectory $unzipLocation
+
+    # Create Desktop shortcuts
+    $desktopShortcutPath = Join-Path ([Environment]::GetFolderPath("Desktop")) "$($_[0]).lnk"
+    Install-ChocolateyShortcut -ShortcutFilePath $desktopShortcutPath -Target $_[1] -WorkingDirectory $unzipLocation
+}
