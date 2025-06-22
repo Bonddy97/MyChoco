2cb40645291199962d142491965cc8f6a2c3ea9e
Bonddy97 <148364322+Bonddy97@users.noreply.github.com>
2025-06-22 16:22:24 +0000

    Update godot-dev

--- Packages/godot-dev/tools/chocolateyinstall.ps1
+++ Packages/godot-dev/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@
 #$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = "Godot-dev"
 # For win32
-$url = 'https://github.com/godotengine/godot-builds/releases/download/4.5-dev5/Godot_v4.5-dev5_win32.exe.zip'
-$checksum = '2ced37914c148b72f4733c65d3d88a675952909473d7c49c99c179ba77f12701'
+$url = 'https://github.com/godotengine/godot-builds/releases/download/4.5-beta1/Godot_v4.5-beta1_win32.exe.zip'
+$checksum = 'd4f60836703252f5a49773b376d653e944d8da98415f5a6934292fc6e373c2a8'
 # For win64
-$url64 = 'https://github.com/godotengine/godot-builds/releases/download/4.5-dev5/Godot_v4.5-dev5_win64.exe.zip'
-$checksum64 = 'b974a70e697d2cb826126328f4a936da0029874154766bb386613585d9dd9c6d'
+$url64 = 'https://github.com/godotengine/godot-builds/releases/download/4.5-beta1/Godot_v4.5-beta1_win64.exe.zip'
+$checksum64 = 'd6259aa5a486978a3ec3a22c16ab0519a178a371d365c761d3289890011314a3'
 # Define unizip location.
 $unzipLocation = Join-Path ([Environment]::GetFolderPath("LocalApplicationData")) $packageName
 
