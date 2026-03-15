7ab4484901907cf8a31824c428e593d40b3b7f97
Bonddy97 <148364322+Bonddy97@users.noreply.github.com>
2026-03-15 16:34:25 +0000

    Update godot-mono-dev

--- Packages/godot-mono-dev/tools/chocolateyinstall.ps1
+++ Packages/godot-mono-dev/tools/chocolateyinstall.ps1
@@ -10,11 +10,11 @@
 #$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = "Godot-mono-dev"
 # For win32
-$url = 'https://github.com/godotengine/godot-builds/releases/download/4.7-dev2/Godot_v4.7-dev2_mono_win32.zip'
-$checksum = 'fa1c57f4bbc24ab0d2982cf5671cc6f408de560e30f4a4bab6cf10cd22d4775c'
+$url = 'https://github.com/godotengine/godot-builds/releases/download/4.6.2-rc1/Godot_v4.6.2-rc1_mono_win32.zip'
+$checksum = '4e71aaa6900b11ea1f2d658fec5d46bd0aac3c2c05ecb40c3f803474c7649d7e'
 # For win64
-$url64 = 'https://github.com/godotengine/godot-builds/releases/download/4.7-dev2/Godot_v4.7-dev2_mono_win64.zip'
-$checksum64 = 'a3440e7c956c5b88d802563fdd1e4a685600cace86dd5065e4a7b9260a4cb917'
+$url64 = 'https://github.com/godotengine/godot-builds/releases/download/4.6.2-rc1/Godot_v4.6.2-rc1_mono_win64.zip'
+$checksum64 = '940a2332707c8c73356d1b9b78281f85cba1f0c485d04ee75ffba97d921b5491'
 # Define unizip location.
 $unzipLocation = Join-Path ([Environment]::GetFolderPath("LocalApplicationData")) $packageName
 
